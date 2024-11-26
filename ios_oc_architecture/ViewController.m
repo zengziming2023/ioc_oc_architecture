1602abe06cf525e897b45fd412a3f4f5d44e8a5d
Nick2022 <876927113@qq.com>
2024-11-26 17:42:22 +0800

    feat: practise UITabBarController.

--- ios_oc_architecture/ViewController.m
+++ ios_oc_architecture/ViewController.m
@@ -16,6 +16,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+//    UILabel *label = [UILabel new];
+//    label.text = @"Hello World!";
+//    [label sizeToFit];
+//    label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+//    
+//    [self.view addSubview: label];
+//    
+//    
+//    NSLog(@"first view controller.");
+        
+    
+    UIViewController *controller1 = [UIViewController new];
+    controller1.view.backgroundColor = [UIColor redColor];
+    controller1.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"新闻" image:[UIImage systemImageNamed:@"house"] selectedImage:[UIImage systemImageNamed:@"house.fill"]];
+    
+    UIViewController *controller2 = [UIViewController new];
+    controller2.view.backgroundColor = [UIColor greenColor];
+    controller2.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"视频" image:[UIImage systemImageNamed:@"gear"] selectedImage:[UIImage systemImageNamed:@"gear.fill"]];
+    
+    UIViewController *controller3 = [UIViewController new];
+    controller3.view.backgroundColor = [UIColor blueColor];
+    controller3.tabBarItem = [[UITabBarItem alloc] initWithTitle: @"推荐" image:[UIImage systemImageNamed:@"gear"] selectedImage:[UIImage systemImageNamed:@"gear.fill"]];
+    
+    UIViewController *controller4 = [UIViewController new];
+    controller4.view.backgroundColor = [UIColor yellowColor];
+    controller4.tabBarItem = [[UITabBarItem alloc] initWithTitle: @"我的" image:[UIImage systemImageNamed:@"gear"] selectedImage:[UIImage systemImageNamed:@"gear.fill"]];
+    
+    [self setViewControllers:@[controller1, controller2, controller3, controller4]];
+    
+    self.tabBar.backgroundColor = [UIColor lightGrayColor];
+
 }
 
 
